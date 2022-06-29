4b099122211430c8a85caf5ca81f6b115144936e
theProgrammingBox <justinboxgea@gmail.com>
2022-06-28 21:31:14 -0700

    g

--- Hw7.cpp
+++ Hw7.cpp
@@ -23,15 +23,22 @@
 
 int main()
 {
-    cout <<"Hello World!" << endl;
-    
+    ManageInventory inventory(10);
+    inventory.addItem("Nike basketball shoes", 22, 145.99);
+    inventory.addItem("Under Armour T-shirt", 33, 29.99);
+    inventory.addItem("Brooks running shoes", 11, 111.44);
+    inventory.addItem("Asics running shoes", 20, 165.88);
+    inventory.addItem("Nike shorts", 77, 45.77);
+
+    cout << inventory << endl;
+
     return 0;
 }
 
 /*************************************************************************
  * Method : Class none
  * ----------------------------------------------------------------------
- * This function will 
+ * This function will
  * ----------------------------------------------------------------------
  * PRE-CONDITIONS
  * 		//
